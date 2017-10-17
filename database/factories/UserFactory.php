db783f470adf793471423067c8950622ff6d369b
Yuleidy Valladares <yuleidyvalladares@gmail.com>
2017-10-17 07:46:40 -0430

    Faker donaciones y usuarios

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,13 +14,39 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
+        'id' => $faker->numberBetween($min = 1000000, $max = 30000000),
+        'nombre' => $faker->firstName,
+        'apellido' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\donacion::class, function (Faker $faker) {
+
+    $nombre = [
+        "Ibritumomab 10mg",
+        "Ibritumomab 50mg",
+        "Ibritumomab 100mg",
+        "Rituximab 2mg",
+        "Rituximab 4mg",
+        "Rituximab 6mg",
+        "Tositumomab 100mg",
+        "Tositumomab 500mg",
+        "Trastuzumab 100mg"
+    ];
+
+    $tipo = [
+        "Medicamento",
+        "Insumo"
+    ];
+    return [
+        'nombre' => $nombre[array_rand($nombre)],
+        'tipo' => $tipo[array_rand($tipo)],
     ];
 });
